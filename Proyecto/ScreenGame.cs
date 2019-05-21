d6f21683c9b25c1ce4691dae411b89f40a1a31fd
Mauricio <mauricio.araujo@protonmail.com>
2019-05-20 22:05:34 -0500

    Dibujar vertices

--- Proyecto/ScreenGame.cs
+++ Proyecto/ScreenGame.cs
@@ -25,22 +25,30 @@ namespace Proyecto
 
             GL.LoadIdentity();
             GL.MatrixMode(MatrixMode.Projection);
-            GL.Ortho(-10, 10, -10, 10, 10, -10);
-            obj = Loader.FromFile("nave.obj");
+            GL.Ortho(-15, 15, -15, 15, 15, -15);
+            obj = Loader.FromFile("AirDrone.obj");
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            
+
+            GL.Rotate(10.5f, 10.5f, 10.5f, 10.5f);
             GL.Begin(PrimitiveType.Triangles);
             foreach (Vector3 vertex in obj.Vertices)
             {
+                int index = obj.Vertices.IndexOf(vertex);
                 GL.Vertex3(vertex);
+
+                if(index < obj.Vertices.ToArray().Length - 2)
+                {
+                    GL.Vertex3(obj.Vertices.ElementAt(index + 1));
+                    GL.Vertex3(obj.Vertices.ElementAt(index + 2));
+                }
             }
             GL.End();
-            
+
             SwapBuffers();
         }
 
